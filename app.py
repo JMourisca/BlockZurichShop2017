c68d2876f4d66ab42663433818d8187c55bb1910
Juliana <jmourisca@gmail.com>
2017-09-16 02:38:03 +0200

    Added templates

--- app.py
+++ app.py
@@ -1,13 +1,18 @@
-from flask import Flask
+from flask import Flask, render_template
 from redis import Redis
+from web3 import Web3, IPCProvider
 
 app = Flask(__name__)
 redis = Redis(host='redis', port=6379)
 
 @app.route('/')
-def hello():
+def index():
+    web3 = Web3(IPCProvider())
+    #
+    # blockNumber = web3.eth.blockNumber
+    blockNumber = 1
     count = redis.incr('hits')
-    return 'Hello World! I have been seen {} times.\n'.format(count)
+    return render_template('index.html', my_string="Wheeeee!", blockNumber=blockNumber, count=count)
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", debug=True)
