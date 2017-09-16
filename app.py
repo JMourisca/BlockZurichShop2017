19cf050750c584f22a026345d90f658220ebe22f
Juliana <jmourisca@gmail.com>
2017-09-16 02:55:26 +0200

    Added post example

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, jsonify
 from redis import Redis
 from web3 import Web3, IPCProvider
 
@@ -13,6 +13,14 @@ def index():
     blockNumber = 1
     count = redis.incr('hits')
     return render_template('index.html', my_string="Wheeeee!", blockNumber=blockNumber, count=count)
+
+@app.route('/postexample', methods=['POST'])
+def post_method():
+    return jsonify(
+        username="HackZurich",
+        email="superapp@gmail.com",
+        id=123
+    )
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", debug=True)
